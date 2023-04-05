6bc1ebeed285b38a9bcc33665db149fdd39dd537
MahiHail <hail.mulat@gmail.com>
2023-04-05 01:42:26 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* _puts_recursion - Write a function that prints a string,
-* followed by a new line.
-*
-* @s: to check
-*
-* return : void
+ * _puts_recursion - functionlike puts();
+ * @s: input
+ * Return: Always 0 (Success)
 */
+
 void _puts_recursion(char *s)
 {
-puts(s);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
