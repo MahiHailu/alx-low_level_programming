19eae4166c2b3f987af5547696c8b833293f64f9
MahiHail <hail.mulat@gmail.com>
2023-04-19 09:37:09 -0700

    Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,25 +1,18 @@
-#include "3-calc.h"
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
-
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- * main - ...
- * @argc: ...
- * @argv: ...
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0 (Success)
- * Return: ...
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
-	int (*oprt)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -27,31 +20,24 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	func = get_op_func(argv[2]);
-	oprt = get_op_func(argv[2]);
-
-	if (!func)
-	if (!oprt)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-	printf("%d\n", result);
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
