83baffdf25950a92f34506916818b3254d53e5b3
MahiHailu <hail.mualt@gmail.com>
2023-04-27 07:27:17 -0700

    all tasks pushed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,21 @@
-section   .text
-	global main
-main:
-	mov       rax, 1
-	mov       rdi, 1
-	mov       rsi, msg
-	mov       rdx, 17
-	syscall
-	mov       rax, 60
-	xor       rdi, rdi
-	syscall
+extern printf
 
-	section   .data
-msg:	db        "Hello, Holberton",0
+section .text
+global main
+
+main:
+push rbp
+
+mov rdi, fmt
+mov rsi, msg
+mov rax, 0
+call printf
+
+pop rbp
+
+mov rax, 0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
