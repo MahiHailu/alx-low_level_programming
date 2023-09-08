8470654688b680d11829463221bc29f4f9f99beb
MahiHailu <hail.mulat@gmail.com>
2023-09-08 04:12:16 -0700

    git commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Create the hash table
+ * hash_table_create - creates a hash table
  * @size: size of the array
  *
- * Return: pointer the newly created hash table
+ * Return: a pointer to the newly created hash table
  * NULL if something went wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
